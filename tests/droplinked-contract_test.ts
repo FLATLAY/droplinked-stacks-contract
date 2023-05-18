482940c1a0724837fd4d48e6be594acbbc8dc7ea
Erfan Yeganegi <76695726+erfanyeganegi@users.noreply.github.com>
2023-05-18 05:23:55 +0330

    fix!: fix tests structures

--- tests/droplinked-contract_test.ts
+++ tests/droplinked-contract_test.ts
@@ -9,7 +9,7 @@ import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts'
 import { uintValue } from './utils/clarity.util.ts'
 
 Clarinet.test({
-	name: 'droplinked:create',
+	name: 'droplinked:create: should update the maps and return the correct result when creating a new SKU',
 	fn(chain: Chain, accounts: Map<string, Account>) {
 		const deployer = accounts.get('deployer')!
 		const creator = accounts.get('wallet_1')!
@@ -34,76 +34,54 @@ Clarinet.test({
 			),
 		])
 
-		Clarinet.test({
-			name: 'should return ok response with sku id',
-			fn: () => {
-				assertEquals(block.receipts[0].result, types.ok(types.uint(1)))
-			},
-		})
+		// droplinked:create should return ok response with sku id
+		assertEquals(block.receipts[0].result, types.ok(types.uint(1)))
 
 		const createdSkuIdResult = block.receipts[0].result.expectOk()
 		const createdSkuId = uintValue(createdSkuIdResult)
 
-		Clarinet.test({
-			name: 'should update commissions map',
-			fn: () => {
-				const commissionResult = chain.callReadOnlyFn(
-					droplinkedContract,
-					'get-commission',
-					[types.uint(createdSkuId), types.principal(creator.address)],
-					creator.address
-				).result
+		// droplinked:create should update commissions map
+		const commissionResult = chain.callReadOnlyFn(
+			droplinkedContract,
+			'get-commission',
+			[types.uint(createdSkuId), types.principal(creator.address)],
+			creator.address
+		).result
+		assertEquals(commissionResult, types.ok(types.some(types.uint(commission))))
 
-				assertEquals(
-					commissionResult,
-					types.ok(types.some(types.uint(commission)))
-				)
-			},
-		})
+		// droplinked:create should update creators map
+		chain
+			.callReadOnlyFn(
+				droplinkedContract,
+				'get-creator',
+				[types.uint(createdSkuId)],
+				creator.address
+			)
+			.result.expectOk()
+			.expectSome()
+			.expectPrincipal(creator.address)
 
-		Clarinet.test({
-			name: 'should update creators map',
-			fn: () => {
-				const creatorsResult = chain.callReadOnlyFn(
-					droplinkedContract,
-					'get-creator',
-					[types.uint(createdSkuId)],
-					creator.address
-				)
+		// droplinked:create should update balances map
+		chain
+			.callReadOnlyFn(
+				droplinkedContract,
+				'get-balance',
+				[types.uint(createdSkuId), types.principal(creator.address)],
+				creator.address
+			)
+			.result.expectOk()
+			.expectUint(amount)
 
-				assertEquals(
-					creatorsResult.result,
-					types.ok(types.some(types.principal(creator.address)))
-				)
-			},
-		})
-
-		Clarinet.test({
-			name: 'should update balances map',
-			fn: () => {
-				const balanceResult = chain.callReadOnlyFn(
-					droplinkedContract,
-					'get-balance',
-					[types.uint(createdSkuId), types.principal(creator.address)],
-					creator.address
-				).result
-
-				assertEquals(balanceResult, types.ok(types.some(types.uint(amount))))
-			},
-		})
-
-		Clarinet.test({
-			name: 'should update prices map',
-			fn: () => {
-				const balanceResult = chain.callReadOnlyFn(
-					droplinkedContract,
-					'get-price',
-					[types.uint(createdSkuId), types.principal(creator.address)],
-					creator.address
-				).result
-
-				assertEquals(balanceResult, types.ok(types.some(types.uint(22))))
-			},
-		})
+		// droplinked:create should update prices map
+		chain
+			.callReadOnlyFn(
+				droplinkedContract,
+				'get-price',
+				[types.uint(createdSkuId)],
+				creator.address
+			)
+			.result.expectOk()
+			.expectSome()
+			.expectUint(price)
 	},
 })
